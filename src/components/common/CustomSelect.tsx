f82f5e707b2717f13ab7e65d9a5cc57e33b4b657
Harsh <harshp8513@gmail.com>
2021-05-17 11:13:47 +0530

    Remove Styled components and use inline style

--- src/components/common/CustomSelect.tsx
+++ src/components/common/CustomSelect.tsx
@@ -1,6 +1,5 @@
-import { TriangleDownIcon } from '@chakra-ui/icons';
-import styled from '@emotion/styled';
-import React, { FC, SyntheticEvent, useState } from 'react'
+import { Box, List, ListItem, Text } from '@chakra-ui/layout';
+import { FC, SyntheticEvent, useState } from 'react'
 
 interface Prpos {
     Options: any[]
@@ -18,73 +17,61 @@ const CustomSelect: FC<Prpos> = (props) => {
         setToggle(false);
     }
     return (
-        <SelectWrap>
-            <SelectTitle onClick={() => setToggle(!toggle)}>{selected}
-            </SelectTitle>
-            <SelectList style={!toggle ? { display: "none" } : {}}>
+        <Box d="flex" pos="relative" flexDirection="column">
+            <Text onClick={() => setToggle(!toggle)}
+                border="1px solid #ccc"
+                padding="8px 10px"
+                position="relative"
+                cursor="pointer"
+                whiteSpace="nowrap"
+                paddingRight="35px"
+                overflow="hidden"
+                borderRadius="5px"
+                backgroundColor="#f3f3f3"
+                fontSize="15px"
+                fontWeight="500"
+                textOverflow="ellipsis"
+                _after={{
+                    position: "absolute",
+                    content: `""`,
+                    height: 0,
+                    width: 0,
+                    borderStyle: "solid",
+                    borderWidth: "6px",
+                    borderColor: "#999 transparent transparent",
+                    right: "10px",
+                    top: "16px",
+                }}
+            > {selected}
+            </Text>
+            <List style={!toggle ? { display: "none" } : {}}
+                listStyle="none"
+                padding="10px"
+                background="#fff"
+                border="1px solid #eee"
+                marginTop="10px"
+                boxShadow="0px 0px 5px #ddd"
+                position="absolute"
+                width="100%"
+                top="100%"
+                border-radius="3px"
+            >
                 {
                     Options.map((option, index) => {
                         if (option !== selected)
-                            return <Option onClick={handleSelect} key={index}>{option}</Option>
+                            return <ListItem
+                                fontSize="14px"
+                                marginTop="10px"
+                                marginBottom="15px"
+                                fontFamily="Montserrat,sans-serif"
+                                fontWeight="500"
+                                cursor=" pointer"
+                                onClick={handleSelect} key={index}>{option}</ListItem>
                     })
                 }
-            </SelectList>
-        </SelectWrap>
+            </List>
+        </Box >
     )
 }
 
 export default CustomSelect
-
-
-
-const SelectWrap = styled.div`
-    display: flex;
-    flex-direction: column;
-    position: relative; `
-
-const SelectIcon = styled.div``
-
-const SelectTitle = styled.p`
-    border: 1px solid #ccc;
-    padding: 8px 10px;
-    position: relative;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    padding-right: 35px;
-    overflow: hidden;
-    border-radius: 5px;
-    background-color: #f3f3f3;
-    cursor: pointer;
-    font-size: 15px;
-    font-weight: 500;
-    &:after {
-        position: absolute;
-        content: "";
-        height: 0;
-        width: 0;
-        border-style: solid;
-        border-width: 6px;
-        border-color: #999 transparent transparent;
-        right: 10px;
-        top: 16px;
-    }`
-
-const SelectList = styled.ul`
-    list-style: none;
-    padding: 10px;
-    background: #fff;
-    border: 1px solid #eee;
-    margin-top: 10px;
-    box-shadow: 0px 0px 5px #ddd;
-    position: absolute;
-    width: 100%;
-    top: 100%;
-    border-radius: 3px; `
-
-const Option = styled.li`
-    font-size: 14px;
-    margin-top: 10px;
-    margin-bottom: 15px;
-    font-family: "Montserrat", sans-serif;
-    font-weight: 500;
-    cursor: pointer;`
